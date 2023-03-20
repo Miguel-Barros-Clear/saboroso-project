04ee3e5829fba3d89707552287f0ed853bb9ebef
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-20 09:53:50 -0300

    feat: adding integration with database in ejs

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ var conn = require('../inc/db')
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   conn.query("SELECT * FROM tb_users ORDER BY name", (err, results) => {
     if (err) {
