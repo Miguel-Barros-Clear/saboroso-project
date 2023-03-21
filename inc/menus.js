5dd664e4af856c0a57aa2a9df159789cb82734ed
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-21 12:30:35 -0300

    feat: adding insert itens in to menu funtion in administration home page

--- inc/menus.js
+++ inc/menus.js
@@ -1,16 +1,40 @@
 let conn = require('./db')
+let path = require('path')
 
 module.exports = {
-    getMenus(){
-        return new Promise((res, rej) => {
-            conn.query(`
+  getMenus() {
+    return new Promise((res, rej) => {
+      conn.query(`
     SELECT * FROM tb_menus ORDER by title
     `, (err, results) => {
-      if(err){
-        rej(err)
-      }
-      res(results)
+        if (err) {
+          rej(err)
+        }
+        res(results)
+      })
     })
-        })
-    }
+  },
+
+  save(fields, files) {
+    return new Promise((res, rej) => {
+
+      fields.photo = `images/${path.parse(files.photo.path).base}`
+
+      conn.query(`
+          INSERT INTO tb_menus (title, description, price, photo)
+          VALUES(?, ?, ?, ?)
+        `, [
+        fields.title,
+        fields.description,
+        fields.price,
+        fields.photo
+      ], (err, results) => {
+        if (err) {
+          rej(err)
+        } else {
+          res(results)
+        }
+      })
+    })
+  }
 }
