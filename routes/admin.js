c98953bd3889c597d7e9351e0973fb98e18dd03e
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-21 11:33:43 -0300

    perf: refactoring side menu

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ var express = require('express');
 let users = require('../inc/users')
 var router = express.Router();
 var conn = require('../inc/db');
+var admin = require('../inc/admin')
 
 router.use((req, res, next) => {
     if (['/login'].indexOf(req.url) === -1 && !req.session.user) {
@@ -11,13 +12,20 @@ router.use((req, res, next) => {
     }
 })
 
+router.use((req, res, next) => {
+    req.menus = admin.getMenus(req);
+    next();
+})
+
 router.get('/logout', function (req, res, next) {
     delete req.session.user;
     res.redirect('/admin/login');
 })
 
 router.get('/', function (req, res, next) {
-    res.render("admin/index")
+    res.render("admin/index", {
+        menus: req.menus
+    })
 })
 
 router.post('/login', function (req, res, next) {
@@ -40,25 +48,34 @@ router.get('/login', function (req, res, next) {
 })
 
 router.get('/contacts', function (req, res, next) {
-    res.render("admin/contacts")
+    res.render("admin/contacts", {
+        menus: req.menus
+    })
 })
 
 router.get('/emails', function (req, res, next) {
-    res.render("admin/emails")
+    res.render("admin/emails", {
+        menus: req.menus
+    })
 })
 
 router.get('/menus', function (req, res, next) {
-    res.render("admin/menus")
+    res.render("admin/menus", {
+        menus: req.menus
+    })
 })
 
 router.get('/reservations', function (req, res, next) {
     res.render("admin/reservations", {
-        date: {}
+        date: {},
+        menus: req.menus
     })
 })
 
 router.get('/users', function (req, res, next) {
-    res.render("admin/users")
+    res.render("admin/users", {
+        menus: req.menus
+    })
 })
 
 module.exports = router;
