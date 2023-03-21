575fbb28571deb828b6055d3dc270186a8f7312e
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-21 11:57:58 -0300

    feat: adding dynamic users informations in administration home page

--- inc/admin.js
+++ inc/admin.js
@@ -1,4 +1,36 @@
+var conn = require('./db')
+
 module.exports = {
+
+    dashboard() {
+        return new Promise((res, rej) => {
+            conn.query(`
+                SELECT
+                    (SELECT 
+                        COUNT(*)
+                    FROM
+                        tb_contacts) AS nrcontacts,
+                    (SELECT
+                        COUNT(*)
+                    FROM
+                        tb_menus) AS nrmenus,
+                    (SELECT
+                        COUNT(*)
+                    FROM
+                        tb_reservations) AS nrreservations,
+                    (SELECT
+                        COUNT(*)
+                    FROM
+                        tb_users) AS nrusers;   
+            `, (err, results) => {
+                if (err) {
+                    rej(err)
+                } else {
+                    res(results[0])
+                }
+            })
+        })
+    },
 
     getParams(req, params) {
         return Object.assign({}, {
