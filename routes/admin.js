47bb93f7342fca264e817c95b5f9fefe1ddf6f4a
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-21 10:27:42 -0300

    feat: adding administration login

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 var express = require('express');
+let users = require('../inc/users')
 var router = express.Router();
 var conn = require('../inc/db');
 
@@ -6,10 +7,23 @@ router.get('/', function (req, res, next) {
     res.render("admin/index")
 })
 
+router.post('/login', function (req, res, next) {
+    if (!req.body.email) {
+        users.render(req, res, "Preencha o campo email!")
+    } else if (!req.body.password) {
+        users.render(req, res, "Preencha o campo senha!")
+    } else {
+        users.login(req.body.email, req.body.password).then((user) => {
+            req.session.user = "Usuario ou senha incorretos";
+            res.redirect('/admin')
+        }).catch((err) => {
+            users.render(req, res, err.message || err)
+        })
+    }
+})
+
 router.get('/login', function (req, res, next) {
-    if (!req.session.views) req.session.views = 0;
-    console.log("SESSION", req.session.views, req.session.views++);
-    res.render("admin/login")
+    users.render(req, res)
 })
 
 router.get('/contacts', function (req, res, next) {
